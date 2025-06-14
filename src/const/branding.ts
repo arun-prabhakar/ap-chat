90936d13097df0fd5660ef7d580add432842ca68
Arun Prabhakar <arunprabhakar.t@gmail.com>
2025-06-15 00:39:09 +0530

    update branding

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -1,13 +1,11 @@
-// the code below can only be modified with commercial license
-// if you want to use it in the commercial usage
-// please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
+export const LOBE_CHAT_CLOUD = 'APChat Cloud';
+
+export const BRANDING_NAME = 'APChat';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'Arun.IM';
 
 export const BRANDING_URL = {
   help: undefined,
@@ -16,14 +14,10 @@ export const BRANDING_URL = {
 };
 
 export const SOCIAL_URL = {
-  discord: 'https://discord.gg/AYFPHvv2jT',
-  github: 'https://github.com/lobehub',
-  medium: 'https://medium.com/@lobehub',
-  x: 'https://x.com/lobehub',
-  youtube: 'https://www.youtube.com/@lobehub',
+
 };
 
 export const BRANDING_EMAIL = {
-  business: 'hello@lobehub.com',
-  support: 'support@lobehub.com',
+  business: 'no-reply@arun.im',
+  support: 'no-reply@arun.im',
 };
